3f057f3d25f1aa5bd5a7e877df83921aa68e350b
Achim Grimm <achim.grimm@neuland-bfi.de>
2021-10-15 20:20:55 +0200

    Make template caching configurable
    Closes #18

--- src/main/java/de/neuland/kafkabridge/infrastructure/templating/TemplateEngineFactory.java
+++ src/main/java/de/neuland/kafkabridge/infrastructure/templating/TemplateEngineFactory.java
@@ -1,5 +1,6 @@
 package de.neuland.kafkabridge.infrastructure.templating;
 
+import de.neuland.kafkabridge.infrastructure.configuration.KafkaBridgeConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.ITemplateEngine;
@@ -8,21 +9,25 @@ import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
+
 @Configuration
 public class TemplateEngineFactory {
     @Bean
-    public ITemplateEngine templateEngine() {
+    public ITemplateEngine templateEngine(KafkaBridgeConfiguration kafkaBridgeConfiguration) {
         var templateEngine = new TemplateEngine();
         templateEngine.addDialect(new Java8TimeDialect());
-        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setTemplateResolver(templateResolver(kafkaBridgeConfiguration));
         return templateEngine;
     }
 
-    private ITemplateResolver templateResolver() {
+    private ITemplateResolver templateResolver(KafkaBridgeConfiguration kafkaBridgeConfiguration) {
         var fileTemplateResolver = new FileTemplateResolver();
-//        fileTemplateResolver.setPrefix("");
-        fileTemplateResolver.setCacheTTLMs(3600000L);
-        fileTemplateResolver.setCacheable(true);
+        kafkaBridgeConfiguration.getMaybeTemplateCacheDuration().peek(cacheDuration -> {
+            fileTemplateResolver.setCacheTTLMs(cacheDuration.toMillis());
+            fileTemplateResolver.setCacheable(true);
+        }).onEmpty(() -> {
+            fileTemplateResolver.setCacheable(false);
+        });
         return fileTemplateResolver;
     }
 }
--- src/test/java/de/neuland/kafkabridge/lib/templating/TemplateRendererTest.java
+++ src/test/java/de/neuland/kafkabridge/lib/templating/TemplateRendererTest.java
@@ -1,16 +1,22 @@
 package de.neuland.kafkabridge.lib.templating;
 
 import de.neuland.kafkabridge.domain.Json;
+import de.neuland.kafkabridge.infrastructure.configuration.KafkaBridgeConfiguration;
 import de.neuland.kafkabridge.infrastructure.json.ObjectMapperFactory;
 import de.neuland.kafkabridge.infrastructure.templating.TemplateEngineFactory;
+import io.vavr.control.Option;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.HashSet;
@@ -18,14 +24,17 @@ import java.util.Set;
 
 import static java.nio.file.Files.createTempFile;
 import static java.nio.file.Files.writeString;
+import static java.time.Duration.ofSeconds;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.mockito.BDDMockito.given;
 
+
+@ExtendWith(MockitoExtension.class)
 class TemplateRendererTest {
     private static final Logger LOG = LoggerFactory.getLogger(TemplateRendererTest.class);
     private static final Set<Path> TEMPLATE_PATHS = new HashSet<>();
 
-    private final TemplateRenderer templateRenderer = new TemplateRenderer(new TemplateEngineFactory().templateEngine(),
-                                                                           new ObjectMapperFactory().objectMapper());
+    @Mock private KafkaBridgeConfiguration configuration;
 
     @AfterAll
     static void deleteTemplatePathFile() {
@@ -35,38 +44,120 @@ class TemplateRendererTest {
     @Test
     void shouldHaveMergedResult() {
         // given
-        var templatePath = givenTemplate("json", """
-                {
-                  "date": [(
-                    ${ #temporals.createDateTime("2021-08-31T20:30:00").atZone(systemDefaultZoneId).minusSeconds(5).toInstant().toEpochMilli() }
-                  )]
-                }""");
+        var templateRenderer = givenTemplateRendererWithoutCaching();
+
+        var templatePath = createTemplate("json", """
+            {
+              "date": [(
+                ${ #temporals.createDateTime("2021-08-31T20:30:00").atZone(systemDefaultZoneId).minusSeconds(5).toInstant().toEpochMilli() }
+              )]
+            }""");
 
         // when
         var result = templateRenderer.render(templatePath,
                                              new Json<>("""
-                                                                {
-                                                                  "id": "1234"
-                                                                }"""));
+                                                            {
+                                                              "id": "1234"
+                                                            }"""));
 
         var dateInEpochMilliseconds = LocalDateTime.parse("2021-08-31T20:29:55")
                                                    .atZone(ZoneId.systemDefault())
                                                    .toInstant()
                                                    .toEpochMilli();
         assertThatJson(result.value()).isEqualTo("""
-                                                         {
-                                                           "date": %d,
-                                                           "id": "1234"
-                                                         }""".formatted(dateInEpochMilliseconds));
+                                                     {
+                                                       "date": %d,
+                                                       "id": "1234"
+                                                     }""".formatted(dateInEpochMilliseconds));
     }
 
-    private Path givenTemplate(String fileExtension,
-                               String content) {
+    @Test
+    void shouldNotCacheTemplates() {
+        // given
+        var templateRenderer = givenTemplateRendererWithoutCaching();
+
+        var templatePath = createTemplate("json", """
+            {
+              "data": "before"
+            }""");
+
+        templateRenderer.render(templatePath,
+                                new Json<>("{}"));
+
+        givenTemplateContent(templatePath, """
+            {
+              "data": "after"
+            }""");
+
+        // when
+        var result = templateRenderer.render(templatePath,
+                                             new Json<>("{}"));
+
+        assertThatJson(result.value()).isEqualTo("""
+                                                     {
+                                                       "data": "after"
+                                                     }""");
+    }
+
+    @Test
+    void shouldCacheTemplates() {
+        // given
+        var templateRenderer = givenTemplateRendererWithCaching();
+
+        var templatePath = createTemplate("json", """
+            {
+              "data": "before"
+            }""");
+
+        templateRenderer.render(templatePath,
+                                new Json<>("{}"));
+
+        givenTemplateContent(templatePath, """
+            {
+              "data": "after"
+            }""");
+
+        // when
+        var result = templateRenderer.render(templatePath,
+                                             new Json<>("{}"));
+
+        assertThatJson(result.value()).isEqualTo("""
+                                                     {
+                                                       "data": "before"
+                                                     }""");
+    }
+
+    private TemplateRenderer givenTemplateRendererWithoutCaching() {
+        return givenTemplateRenderer(Option.none());
+    }
+
+    private TemplateRenderer givenTemplateRendererWithCaching() {
+        return givenTemplateRenderer(Option.of(ofSeconds(30)));
+    }
+
+    private TemplateRenderer givenTemplateRenderer(Option<Duration> maybeCacheDuration) {
+        given(configuration.getMaybeTemplateCacheDuration()).willReturn(maybeCacheDuration);
+
+        return new TemplateRenderer(new TemplateEngineFactory().templateEngine(configuration),
+                                    new ObjectMapperFactory().objectMapper());
+    }
+
+    private Path createTemplate(String fileExtension,
+                                String content) {
         try {
             var templatePath = createTempFile("kafkabridge-" + TemplateRendererTest.class.getSimpleName(), "." + fileExtension);
             TEMPLATE_PATHS.add(templatePath);
-            writeString(templatePath, content);
+            givenTemplateContent(templatePath, content);
             return templatePath;
+        } catch (IOException e) {
+            throw new RuntimeException("Error on writing template", e);
+        }
+    }
+
+    private void givenTemplateContent(Path templatePath,
+                                      String content) {
+        try {
+            writeString(templatePath, content);
         } catch (IOException e) {
             throw new RuntimeException("Error on writing template", e);
         }
